20574943299fe554798eb10f186a04786f3a8429
denie25 <63772853+denie25@users.noreply.github.com>
2022-01-11 14:52:10 +0700

    Add files via upload

---
+++ Backend_Test/Task1.js
@@ -0,0 +1,10 @@
+let numbers = {
+  a: 2,
+  b: 4
+ };
+
+function result(numbers) {
+   [a, b]= [b, a]
+}
+ 
+console.log(result(numbers));
---
+++ Backend_Test/Task2.js
@@ -0,0 +1,7 @@
+const data = {i:6, j=null, k:3, l:12};
+
+function result(data) {
+
+}
+
+console.log(result(data));
